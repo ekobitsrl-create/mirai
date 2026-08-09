78c941f6daa966efdf7c17e357c531751e70cfe1
ekobitsrl-create <272302019+ekobitsrl-create@users.noreply.github.com>
2026-08-09 15:35:25 +0200

    Sostituisce favicon v0 con MIRAI

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.svg', type: 'image/svg+xml', sizes: 'any' },
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/favicon.png', type: 'image/png', sizes: '512x512' },
